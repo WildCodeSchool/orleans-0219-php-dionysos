daf532af329baf9ac96f15af1e4917df735f4e95
Casey Heagerty <cheagerty@gmail.com>
2019-04-10 16:00:37 +0200

    Initialisation du site avec mise en place de Bootstrap, appel de Twig, et corrections demandé par CodeSniffer.

--- public/index.php
+++ public/index.php
@@ -10,3 +10,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
+
+$loader = new Twig\Loader\FilesystemLoader('../src/View');
+$twig = new Twig\Environment($loader);
+echo $twig->render('index.html.twig');
