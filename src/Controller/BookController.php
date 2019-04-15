833869fb19cc094fb95b00ffabd07b39fc5d97b9
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-04-15 15:54:40 +0200

    Squelette html réservation

---
+++ src/Controller/BookController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+use App\Model\BookManager;
+
+class BookController extends AbstractController
+{
+
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+
+    public function index()
+    {
+        return $this->twig->render('Book/index.html.twig');
+    }
+}
---
+++ src/Model/BookManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class BookManager extends AbstractManager
+{
+
+}
