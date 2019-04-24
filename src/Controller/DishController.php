6e07faf10e538f1b9368f9426e4e085b9f1dacac
Casey Heagerty <cheagerty@gmail.com>
2019-04-24 16:16:57 +0200

    Afficher les plats de la carte sur une page dédiée

--- src/Controller/DishController.php
+++ src/Controller/DishController.php
@@ -9,7 +9,7 @@ class DishController extends AbstractController
 
 
     /**
-     * Display item listing
+     * Display dish listing
      *
      * @return string
      * @throws \Twig\Error\LoaderError
@@ -25,80 +25,4 @@ class DishController extends AbstractController
     }
 
 
-    /**
-     * Display item informations specified by $id
-     *
-     * @param int $id
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function show(int $id)
-    {
-        $dishManager = new DishManager();
-        $dish = $dishManager->selectOneById($id);
-
-        return $this->twig->render('Dish/show.html.twig', ['dish' => $dish]);
-    }
-
-
-    /**
-     * Display item edition page specified by $id
-     *
-     * @param int $id
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function edit(int $id): string
-    {
-        $dishManager = new DishManager();
-        $dish = $dishManager->selectOneById($id);
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $dish['dish_name'] = $_POST['dish_name'];
-            $dishManager->update($dish);
-        }
-
-        return $this->twig->render('Dish/edit.html.twig', ['dish' => $dish]);
-    }
-
-
-    /**
-     * Display item creation page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
-     */
-    public function add()
-    {
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $dishManager = new DishManager();
-            $dish = [
-                'dish_name' => $_POST['dish_name'],
-            ];
-            $id = $dishManager->insert($dish);
-            header('Location:/dish/show/' . $id);
-        }
-
-        return $this->twig->render('Dish/add.html.twig');
-    }
-
-
-    /**
-     * Handle dish deletion
-     *
-     * @param int $id
-     */
-    public function delete(int $id)
-    {
-        $dishManager = new DishManager();
-        $dishManager->delete($id);
-        header('Location:/dish/index');
-    }
 }
--- src/Model/DishManager.php
+++ src/Model/DishManager.php
@@ -22,46 +22,11 @@ class DishManager extends AbstractManager
     }
 
 
-    /**
-     * @param array $dish
-     * @return int
-     */
-    public function insert(array $dish): int
+    public function selectDish(): array
     {
-        // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO $this->table (`title`) VALUES (:title)");
-        $statement->bindValue('dish_name', $dish['dish_name'], \PDO::PARAM_STR);
-
-        if ($statement->execute()) {
-            return (int)$this->pdo->lastInsertId();
-        }
+        return $this->pdo->query('SELECT * FROM ' . $this->table)->fetchAll();
     }
 
 
-    /**
-     * @param int $id
-     */
-    public function delete(int $id): void
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }
-
-
-    /**
-     * @param array $item
-     * @return bool
-     */
-    public function update(array $item):bool
-    {
-
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE $this->table SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-
-        return $statement->execute();
-    }
 }
+
