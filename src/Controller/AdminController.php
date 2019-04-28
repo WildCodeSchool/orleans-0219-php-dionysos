a235496f36cc1242a1835252e95fd3dc551171ab
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-04-28 12:52:33 +0200

    maj d'admin, appel dans layout-admin UNIQUEMENT de admincss et pas style.css, tableau fonctionnel manque update et css

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,7 +7,7 @@
  */
 
 namespace App\Controller;
-
+use App\Model\AdminReviewManager;
 class AdminController extends AbstractController
 {
 
@@ -22,5 +22,32 @@ class AdminController extends AbstractController
     public function index()
     {
         return $this->twig->render('Admin/index.html.twig');
+    }
+
+    /**
+     * Displays all rows ordered by date
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function adminGuestBook()
+    {
+        $adminReviewManager = new adminReviewManager();
+        $reviews = $adminReviewManager->selectAllAdminReviews();
+        return $this->twig->render('Admin/guestbook.html.twig', ['reviews' => $reviews]);
+    }
+
+    /**
+     * Handle item deletion
+     *
+     * @param int $id
+     */
+    public function delete(int $id)
+    {
+        $adminReviewManager = new adminReviewManager();
+        $adminReviewManager ->delete($id);
+        header('Location: Admin/guestbook.html.twig');
     }
 }
