3320bd1075ca7bc6dd18805aaa6c5e57a1d0ddf0
Solène Rambaut <rambaut.solene@yahoo.fr>
2019-04-30 16:02:15 +0200

    Retrait du toggle label, du var_dump...

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -17,23 +17,8 @@ class AdminCategoryController extends AbstractController
      */
     public function index()
     {
-        //base
         $categoryManager = new categoryManager();
         $categories = $categoryManager->selectAll();
-        //fin base
-        //update
-        $data = [];
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            var_dump($_POST);
-
-            foreach ($_POST as $key => $value) {
-                $data[$key] = trim($value);
-            }
-            if (!empty($_POST['label'])) {
-                $categoryManager -> updateLabel($data);
-                header('location:/AdminCategory/index/?success=true&id=' . $data['label'] . '#' . $data['label']);
-            }
-        }
-        return $this->twig->render('Admin/Dish/category.html.twig', ['categories' => $categories, 'category' => $data]);
+        return $this->twig->render('Admin/Dish/category.html.twig', ['categories' => $categories]);
     }
 }
--- src/Model/CategoryManager.php
+++ src/Model/CategoryManager.php
@@ -27,16 +27,4 @@ class CategoryManager extends AbstractManager
         return $this->pdo->query('SELECT * FROM ' . self::TABLE . ' ORDER BY display_order ASC')->fetchAll();
     }
 
-    /**
-     * online from 0 to 1 or 1 to 0
-     * @param array $data
-     * @return bool
-     */
-    public function updateLabel(array $data):bool
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE $this->table SET `label` = !`label` WHERE id=:label");
-        $statement->bindValue(':label', $data['label'], \PDO::PARAM_INT);
-        return $statement->execute();
-    }
 }
